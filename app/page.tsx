8be3da404da3517226048e5a9f71a903555ddd28
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-09 18:25:19 +0530

    installing shadcn and tailwind animation and adding logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className= "text-center underline mt-5">Home</div>
-  );
+  return <div className="flex h-screen max-h-screen">
+    <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+        <Image 
+          src='/assets/icons/logo-full.svg'
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="mb-12 h-10 w-fit"
+        />
+      </div>
+    </section>
+    </div>;
 }
